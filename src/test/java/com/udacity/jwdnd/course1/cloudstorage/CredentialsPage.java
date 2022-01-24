d1496c36cf25565e290d203588bf973766fa6a3d
Gabriela Lopes <gabrieladslopes@gmail.com>
2022-01-24 14:01:22 -0300

    Testing credentials create, edit, delete

---
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/CredentialsPage.java
@@ -0,0 +1,162 @@
+package com.udacity.jwdnd.course1.cloudstorage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class CredentialsPage {
+
+    @FindBy(id = "nav-credentials-tab")
+    private WebElement credentialsTab;
+
+    @FindBy(id = "buttonNewCredential")
+    private WebElement buttonNewCredential;
+
+    @FindBy(id = "credential-url")
+    private WebElement credentialUrl;
+
+    @FindBy(id = "credential-username")
+    private WebElement credentialUsername;
+
+    @FindBy(id = "credential-password")
+    private WebElement credentialPassword;
+
+    @FindBy(id = "buttonSaveChangesCredentials")
+    private WebElement buttonSaveChanges;
+
+    @FindBy(id = "credentialTable")
+    private WebElement table;
+
+    public CredentialsPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    public void createNewCredential(WebDriver driver, String url, String username, String password) {
+        WebDriverWait wait = new WebDriverWait(driver, 1000);
+
+        wait.until(ExpectedConditions.elementToBeClickable(credentialsTab));
+        this.credentialsTab.click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(buttonNewCredential));
+        this.buttonNewCredential.click();
+
+        wait.until(ExpectedConditions.visibilityOf(credentialUrl));
+        this.credentialUrl.sendKeys(url);
+        this.credentialUsername.sendKeys(username);
+        this.credentialPassword.sendKeys(password);
+        this.buttonSaveChanges.click();
+    }
+
+    public boolean credentialExists(WebDriver driver, String url, String username, String password) {
+        WebDriverWait wait = new WebDriverWait(driver, 1000);
+
+        wait.until(ExpectedConditions.elementToBeClickable(credentialsTab));
+        this.credentialsTab.click();
+
+        List<WebElement> credentialTableRows = table.findElements(By.className("credentialTableRow"));
+
+        for (WebElement credentialTableRow : credentialTableRows) {
+            WebElement urlElement = credentialTableRow.findElement(By.className("classCredentialUrl"));
+            wait.until(ExpectedConditions.visibilityOf(urlElement));
+
+            WebElement usernameElement = credentialTableRow.findElement(By.className("classCredentialUsername"));
+            wait.until(ExpectedConditions.visibilityOf(usernameElement));
+
+            WebElement passwordElement = credentialTableRow.findElement(By.className("classCredentialPassword"));
+            wait.until(ExpectedConditions.visibilityOf(passwordElement));
+
+            if(urlElement.getText().equals(url) &&
+                    usernameElement.getText().equals(username) &&
+                    !passwordElement.getText().equals(password)) return true;
+        }
+
+        return false;
+    }
+
+    public String getEncryptedPassword(WebDriver driver, String url, String username) {
+        WebDriverWait wait = new WebDriverWait(driver, 1000);
+        String encryptedPassword = "";
+
+        wait.until(ExpectedConditions.elementToBeClickable(credentialsTab));
+        this.credentialsTab.click();
+
+        List<WebElement> credentialTableRows = table.findElements(By.className("credentialTableRow"));
+
+        for (WebElement credentialTableRow : credentialTableRows) {
+            WebElement urlElement = credentialTableRow.findElement(By.className("classCredentialUrl"));
+            wait.until(ExpectedConditions.visibilityOf(urlElement));
+
+            WebElement usernameElement = credentialTableRow.findElement(By.className("classCredentialUsername"));
+            wait.until(ExpectedConditions.visibilityOf(usernameElement));
+
+            WebElement passwordElement = credentialTableRow.findElement(By.className("classCredentialPassword"));
+            wait.until(ExpectedConditions.visibilityOf(passwordElement));
+
+            if(urlElement.getText().equals(url) &&
+                    usernameElement.getText().equals(username)) {
+                encryptedPassword = passwordElement.getText();
+            }
+        }
+        return encryptedPassword;
+    }
+
+    public void editPasswordCredential(WebDriver driver, String url, String username, String newPassword) {
+        WebDriverWait wait = new WebDriverWait(driver, 1000);
+
+        wait.until(ExpectedConditions.elementToBeClickable(credentialsTab));
+        this.credentialsTab.click();
+
+        List<WebElement> credentialTableRows = table.findElements(By.className("credentialTableRow"));
+
+        for (WebElement credentialTableRow : credentialTableRows) {
+
+            WebElement urlElement = credentialTableRow.findElement(By.className("classCredentialUrl"));
+            wait.until(ExpectedConditions.visibilityOf(urlElement));
+
+            WebElement usernameElement = credentialTableRow.findElement(By.className("classCredentialUsername"));
+            wait.until(ExpectedConditions.visibilityOf(usernameElement));
+
+            if(urlElement.getText().equals(url) && usernameElement.getText().equals(username)) {
+                WebElement editButton = credentialTableRow.findElement(By.tagName("button"));
+                wait.until(ExpectedConditions.elementToBeClickable(editButton));
+                editButton.click();
+
+                wait.until(ExpectedConditions.visibilityOf(credentialPassword));
+                this.credentialPassword.clear();
+                this.credentialPassword.sendKeys(newPassword);
+                this.buttonSaveChanges.click();
+            }
+        }
+    }
+
+    public void deleteCredential(WebDriver driver, String url, String username) {
+        WebDriverWait wait = new WebDriverWait(driver, 1000);
+
+        wait.until(ExpectedConditions.elementToBeClickable(credentialsTab));
+        this.credentialsTab.click();
+
+        List<WebElement> credentialTableRows = table.findElements(By.className("credentialTableRow"));
+
+        for (WebElement credentialTableRow : credentialTableRows) {
+            WebElement urlElement = credentialTableRow.findElement(By.className("classCredentialUrl"));
+            wait.until(ExpectedConditions.visibilityOf(urlElement));
+            String urlElementText = urlElement.getText();
+
+            WebElement usernameElement = credentialTableRow.findElement(By.className("classCredentialUsername"));
+            wait.until(ExpectedConditions.visibilityOf(usernameElement));
+            String usernameElementText = usernameElement.getText();
+
+            if(urlElementText.equals(url) && usernameElementText.equals(username)) {
+                WebElement deleteButton = credentialTableRow.findElement(By.tagName("a"));
+                wait.until(ExpectedConditions.elementToBeClickable(deleteButton));
+                deleteButton.click();
+            }
+        }
+    }
+}
---
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/CredentialsTest.java
@@ -0,0 +1,112 @@
+package com.udacity.jwdnd.course1.cloudstorage;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CredentialsTest {
+
+    @LocalServerPort
+    private int port;
+
+    private WebDriver driver;
+
+    @BeforeAll
+    static void beforeAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        this.driver = new ChromeDriver();
+
+        String firstName = "John";
+        String lastName = "Robert";
+        String username = "john";
+        String password = "12345";
+
+        driver.get("http://localhost:" + port + "/signup");
+        SignupPage signupPage = new SignupPage(driver);
+        signupPage.signUp(firstName, lastName, username, password);
+
+        driver.get("http://localhost:" + port + "/login");
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login(username, password);
+    }
+
+    @AfterEach
+    public void afterAll() {
+        if(this.driver != null) {
+            this.driver.quit();
+        }
+    }
+
+    @Test
+    public void testCredentialCreationDisplayedAndPasswordEncrypted() throws InterruptedException {
+
+        for(int i = 0; i < 5; i++) {
+            String url = "www.urlteste" + i + ".com";
+            String username = "John" + i;
+            String password = "john" + i;
+
+            CredentialsPage credentialsPage = new CredentialsPage(driver);
+            credentialsPage.createNewCredential(driver, url, username, password);
+
+            driver.get("http://localhost:" + port + "/home");
+            assertTrue(credentialsPage.credentialExists(driver, url, username, password));
+
+            Thread.sleep(5000);
+        }
+    }
+
+    @Test
+    public void testCredentialViewEditAndVerifyChanges() throws InterruptedException {
+
+        String url = "www.urlteste.com";
+        String username = "John";
+        String password = "john";
+
+        CredentialsPage credentialsPage = new CredentialsPage(driver);
+        credentialsPage.createNewCredential(driver, url, username, password);
+
+        driver.get("http://localhost:" + port + "/home");
+
+        String encryptedPassword = credentialsPage.getEncryptedPassword(driver, url, username);
+
+        String newPassword = "john1";
+        credentialsPage.editPasswordCredential(driver, url, username, newPassword);
+
+        driver.get("http://localhost:" + port + "/home");
+        String newEncryptedPassword = credentialsPage.getEncryptedPassword(driver, url, username);
+
+        assertNotEquals(encryptedPassword, newEncryptedPassword);
+    }
+
+    @Test
+    public void testCredentialDeletion() {
+
+        CredentialsPage credentialsPage = new CredentialsPage(driver);
+
+        String url = "www.urlteste.com";
+        String username = "John";
+        String password = "john";
+
+        driver.get("http://localhost:" + port + "/home");
+        credentialsPage.createNewCredential(driver, url, username, password);
+
+        driver.get("http://localhost:" + port + "/home");
+        credentialsPage.deleteCredential(driver, url, username);
+
+        driver.get("http://localhost:" + port + "/home");
+        assertFalse(credentialsPage.credentialExists(driver, url, username, password));
+    }
+}
